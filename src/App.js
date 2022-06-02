ecf8d5824b1e6692f853afddd2b0e87feb805e30
ezequiel magi <ezequielmaggi@gmail.com>
2022-06-02 12:38:06 -0300

    inicializando beta de encuestas

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        
+        <img src={'https://i.postimg.cc/RCnGsnkP/ITCpng.png'}  alt="avatar de la doña"/>
+        
+        <p>Ayudanos a mejorar tu experiencia en clase respondiendo estas breves preguntas:</p>
+        
+        <button><a href="https://form.typeform.com/to/lpROXHCg">Encuesta</a></button>
+      
+      </div>
+    </>
+  );
+}
+
+export default App;
